4f495c720f630bc3d4e17ba2d5354ba280a1e80c
hemilioaraujo <hemilioaraujo@gmail.com>
2025-04-11 19:21:38 -0300

    feat(Tipos): CRUD

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\AtivoController;
 use App\Http\Controllers\CorretoraController;
+use App\Http\Controllers\TipoController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('ativos', AtivoController::class);
 Route::apiResource('corretoras', CorretoraController::class);
+Route::apiResource('tipos', TipoController::class);
